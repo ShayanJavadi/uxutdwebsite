0a7af570f8dec3cba15fcafe7a58028c0cec0add
shayanjavadi <shayanjavadi1375@gmail.com>
2017-12-17 22:39:47 -0600

    update

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require('./styles/app.scss');
 document.addEventListener('DOMContentLoaded', function() {
   const App = new Application;
   App.handleAnchorNavigation();
+  App.handleEmbeddedSocialMedia();
 }, false);
 
 
@@ -34,4 +35,22 @@ class Application {
     })
   }
 
+  handleEmbeddedSocialMedia() {
+    // TWITTER
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // FACEBOOK
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.async = true;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=122107125107711';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
 }
